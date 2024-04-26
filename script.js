f359261466225de590178775afd4203a28dc986b
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-04-25 18:41:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function buypass() {
-    new PaymentRequest(
+
+function initPaymentRequest() {
+    return new PaymentRequest(
         [
             {
                 supportedMethods: location.origin + "/pay/main.json",
@@ -15,4 +16,66 @@ function buypass() {
     ).show();
 }
 
-document.querySelector("button").onclick = buypass;
+/**
+ * Invokes PaymentRequest for the BobBucks payment method.
+ *
+ * @param {PaymentRequest} request The PaymentRequest object.
+ */
+function onBuyClicked(request) {
+  request.show().then(function(instrumentResponse) {
+    sendPaymentToServer(instrumentResponse);
+  })
+  .catch(function(err) {
+    ChromeSamples.setStatus(err);
+  });
+}
+
+/**
+ * Simulates processing the payment data on the server.
+ *
+ * @param {PaymentResponse} instrumentResponse The payment information to
+ * process.
+ */
+function sendPaymentToServer(instrumentResponse) {
+  // There's no server-side component of these samples. No transactions are
+  // processed and no money exchanged hands. Instantaneous transactions are not
+  // realistic. Add a 2 second delay to make it seem more real.
+  window.setTimeout(function() {
+    instrumentResponse.complete('success')
+        .then(function() {
+          document.getElementById('result').innerHTML =
+              instrumentToJsonString(instrumentResponse);
+        })
+        .catch(function(err) {
+          ChromeSamples.setStatus(err);
+        });
+  }, 2000);
+}
+
+/**
+ * Converts the payment instrument into a JSON string.
+ *
+ * @private
+ * @param {PaymentResponse} instrument The instrument to convert.
+ * @return {string} The JSON string representation of the instrument.
+ */
+function instrumentToJsonString(instrument) {
+  let details = instrument.details;
+
+  return JSON.stringify({
+    methodName: instrument.methodName,
+    details: details,
+  }, undefined, 2);
+}
+
+const payButton = document.getElementById('button');
+if (window.PaymentRequest) {
+  let request = initPaymentRequest();
+  payButton.setAttribute('style', 'display: inline;');
+  payButton.addEventListener('click', function() {
+    onBuyClicked(request);
+    request = initPaymentRequest();
+  });
+} else {
+  ChromeSamples.setStatus('This browser does not support web payments');
+}
