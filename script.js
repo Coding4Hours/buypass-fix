c9ef1428e616682208bca04725346a29e7ad9fc1
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-06 16:54:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function buypass() {
 
 const supportedInstruments = [
   {
-            supportedMethods: "/pay/main.json",
+            supportedMethods: location.origin + "/pay/main.json",
       data:{url:document.querySelector("input").value},
   },
 ];
